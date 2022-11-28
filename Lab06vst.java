5fa0f19213577faa280ae0ffc5f04300c2669bb1
15718 <aden.bradshaw@vtsd.com>
2022-11-28 13:15:04 -0500

    1312544

--- Lab06vst.java
+++ Lab06vst.java
@@ -19,18 +19,62 @@ public class Lab06vst extends Applet
 
 
         // Draw Random Lines
+        Random rndInt = new Random(12345);
+        for (int k= 1; k <= 100; k++)
+
+        {
+
+            int x1 = rndInt.nextInt(391)+10;
+            int y1 = rndInt.nextInt(291)+10;
+            int x2 = rndInt.nextInt(391)+10;
+            int y2 = rndInt.nextInt(291)+10;
+            int red = rndInt.nextInt(256);
+            int green =  rndInt.nextInt(256);
+            int blue =  rndInt.nextInt(256);
+            g.setColor(new Color(red,green,blue));
+            g.drawLine(x1,y1,x2,y2);
+
+
+
+
+        }
 
 
 
 
 
         // Draw Random Squares
+         for (int k= 1; k<= 100; k++)
+
+        {
+
+            int x1 = rndInt.nextInt(341)+400;
+            int y1 = rndInt.nextInt(241)+10;
+            int width = 50;
+            int red = rndInt.nextInt(256);
+            int green =  rndInt.nextInt(256);
+            int blue =  rndInt.nextInt(256);
+            g.setColor(new Color(red,green,blue));
+            g.fillRect(x1,y1,width,width);
+
+
+
+
+        }
 
 
 
 
 
         // Draw Random Circles
+
+
+
+
+
+
+
+
 
 
 
